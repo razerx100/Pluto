0858e683a953710362cfef5242729916a10ce8d4
razerx100 <saikatmandal8@gmail.com>
2021-11-23 13:06:53 +0600

    cleanup bug fixed

--- src/StaticInitialization.cpp
+++ src/StaticInitialization.cpp
@@ -4,16 +4,18 @@
 
 static InputManager* s_pInputManager = nullptr;
 
-InputManager* _cdecl GetInputManagerInstance() noexcept {
+InputManager* GetInputManagerInstance() noexcept {
 	return s_pInputManager;
 }
 
-void _cdecl InitInputManagerInstance() {
+void InitInputManagerInstance() {
 	if (!s_pInputManager)
 		s_pInputManager = new InputManagerImpl;
 }
 
-void _cdecl CleanUpInputManagerInstance() noexcept {
-	if (s_pInputManager)
+void CleanUpInputManagerInstance() noexcept {
+	if (s_pInputManager) {
 		delete s_pInputManager;
+		s_pInputManager = nullptr;
+	}
 }
